583b2ad563374bff1f2590ac9f6bd514e04119e5
Sergio <a20202137@pucp.edu.pe>
2024-10-30 20:59:43 -0500

    ultimo commit

--- src/main/java/com/example/lab8/controller/StudentController.java
+++ src/main/java/com/example/lab8/controller/StudentController.java
@@ -17,36 +17,6 @@ public class StudentController {
         return studentRepository.findByFacultyOrderByGpaDesc(faculty);
     }
 
-    @PostMapping
-    public Student addStudent(@RequestBody Student student) {
-        student.setRankin(1);
-        Student savedStudent = studentRepository.save(student);
-        updateRanks(student.getFaculty());
-        return savedStudent;
-    }
 
-    @PutMapping("/{id}")
-    public Student updateGpa(@PathVariable Integer id, @RequestParam double gpa) {
-        Student student = studentRepository.findById(id).orElseThrow();
-        student.setGpa(gpa);
-        studentRepository.save(student);
-        updateRanks(student.getFaculty());
-        return student;
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable Integer id) {
-        Student student = studentRepository.findById(id).orElseThrow();
-        studentRepository.delete(student);
-        updateRanks(student.getFaculty());
-    }
-
-    private void updateRanks(String faculty) {
-        List<Student> students = studentRepository.findByFacultyOrderByGpaDesc(faculty);
-        for (int i = 0; i < students.size(); i++) {
-            students.get(i).setRankin(i + 1);
-            studentRepository.save(students.get(i));
-        }
-    }
 }
 
